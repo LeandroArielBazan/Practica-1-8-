32e2caf86b7536f2361b11a3123797c828534b07
Leandro Ariel Bazan <leanbazan99@gmail.com>
2023-04-18 12:33:10 -0300

    practica 1-8

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log( 4>4);
+console.log( 23 == 33);
+console.log(37 >= -3);
+console.log("Seba" == "Seba");
+console.log("seba" == "Seba");
+console.log("s7829s;SA" != "123querty");
+console.log( ("seba" == "Seba") && ("s7829s;SA" == "s7829s;SA") );
+console.log( ("seba" == "Seba") || ("s7829s;SA" == "s7829s;SA") );
+console.log( ( "seba" == "Seba" ) && ("s7829s;SA" != "123querty"));
+
+let edad= 25;
+console.log( edad >=18 && edad <100);
+console.log( edad >=18 || edad <100);
+console.log( edad <= 18 || edad <100);
+console.log( edad < 18 && edad > 100);
+console.log( edad < 18 || edad <100);
